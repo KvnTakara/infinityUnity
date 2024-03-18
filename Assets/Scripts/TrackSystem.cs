dfae9632250c9d671579990a455ab9d1ae897b4b
KvnTakara <kvntakara@gmail.com>
2024-03-18 11:38:54 -0300

    Obstacles Optimized

--- Assets/Scripts/TrackSystem.cs
+++ Assets/Scripts/TrackSystem.cs
@@ -14,46 +14,13 @@ public class TrackSystem : MonoBehaviour
 
     List<GameObject> randomTracks = new List<GameObject>();
 
-    public GameObject t0obstacle0;
-    public GameObject t0obstacle1;
+    public GameObject obstacle0;
+    public GameObject obstacle1;
+    public GameObject obstacle2;
+    public GameObject obstacle3;
 
-    public GameObject t1obstacle0;
-    public GameObject t1obstacle1;
-
-    public GameObject t2obstacle0;
-    public GameObject t2obstacle1;
-
-    public GameObject t3obstacle0;
-    public GameObject t3obstacle1;
-    public GameObject t3obstacle2;
-    public GameObject t3obstacle3;
-
-    public GameObject t4obstacle0;
-    public GameObject t4obstacle1;
-    public GameObject t4obstacle2;
-    public GameObject t4obstacle3;
-
-    public GameObject t5obstacle0;
-    public GameObject t5obstacle1;
-    public GameObject t5obstacle2;
-    public GameObject t5obstacle3;
-
-    public GameObject t6obstacle0;
-    public GameObject t6obstacle1;
-    public GameObject t6obstacle2;
-    public GameObject t6obstacle3;
-
-    public GameObject t7obstacle0;
-    public GameObject t7obstacle1;
-    public GameObject t7obstacle2;
-    public GameObject t7obstacle3;
-
-    public GameObject t8obstacle0;
-    public GameObject t8obstacle1;
-    public GameObject t8obstacle2;
-    public GameObject t8obstacle3;
-
-    List<List<GameObject>> obstaclesLists = new List<List<GameObject>>();
+    List<GameObject> obstaclesList = new List<GameObject>();
+    List<Vector3> obstaclesPosition = new List<Vector3>();
 
     void Start()
     {
@@ -62,36 +29,47 @@ public class TrackSystem : MonoBehaviour
         randomTracks.Add(track2);
         randomTracks.Add(track3);
 
-        obstaclesLists.Add(new List<GameObject> { null, t0obstacle0, t0obstacle1});
-        obstaclesLists.Add(new List<GameObject> { null, t1obstacle0, t1obstacle1 });
-        obstaclesLists.Add(new List<GameObject> { null, t2obstacle0, t2obstacle1 });
-        obstaclesLists.Add(new List<GameObject> { null, t3obstacle0, t3obstacle1, t3obstacle2, t3obstacle3 });
-        obstaclesLists.Add(new List<GameObject> { null, t4obstacle0, t4obstacle1, t4obstacle2, t4obstacle3 });
-        obstaclesLists.Add(new List<GameObject> { null, t5obstacle0, t5obstacle1, t5obstacle2, t5obstacle3 });
-        obstaclesLists.Add(new List<GameObject> { null, t6obstacle0, t6obstacle1, t6obstacle2, t6obstacle3 });
-        obstaclesLists.Add(new List<GameObject> { null, t7obstacle0, t7obstacle1, t7obstacle2, t7obstacle3 });
-        obstaclesLists.Add(new List<GameObject> { null, t8obstacle0, t8obstacle1, t8obstacle2, t8obstacle3 });
+        obstaclesList.Add(null);
+        obstaclesList.Add(obstacle0);
+        obstaclesList.Add(obstacle1);
+        obstaclesList.Add(obstacle2);
+        obstaclesList.Add(obstacle3);
+
+        obstaclesPosition.Add(new Vector3(-2, 0, 0));
+        obstaclesPosition.Add(new Vector3(0, 0, 0));
+        obstaclesPosition.Add(new Vector3(2, 0, 0));
+        obstaclesPosition.Add(new Vector3(-2, 0, 10));
+        obstaclesPosition.Add(new Vector3(0, 0, 10));
+        obstaclesPosition.Add(new Vector3(2, 0, 10));
+        obstaclesPosition.Add(new Vector3(-2, 0, 20));
+        obstaclesPosition.Add(new Vector3(0, 0, 20));
+        obstaclesPosition.Add(new Vector3(2, 0, 20));
     }
 
     void InstantiateTrack()
     {
         // Randomize one of the Tracks in randomTracks List to Instantiate.
-        int randomNum = Random.Range(0, randomTracks.Count);
-        GameObject selectedTrack = randomTracks[randomNum];
+        int randomIndex = Random.Range(0, randomTracks.Count);
+        GameObject selectedTrack = randomTracks[randomIndex];
         GameObject trackInstance = Instantiate(selectedTrack, targetPosition, Quaternion.identity);
 
-        // Pick randomly one of the tree rail tracks to Instantiate a random Obstacle.
         int rangeMin = 0;
         int rangeMax = 2;
         for (int i = 0;i < 2; i++)
         {
-            randomNum = Random.Range(rangeMin, rangeMax);
-            int randomNum2 = Random.Range(0, obstaclesLists[randomNum].Count);
-            GameObject selectedObstacle = obstaclesLists[randomNum][randomNum2];
-            if (obstaclesLists[randomNum][randomNum2] != null)
+            // Pick a random obstacle in the list and then a random position for it.
+            if (i == 0) randomIndex = Random.Range(0, 2);
+            else randomIndex = Random.Range(0, obstaclesList.Count);
+            int randomPosition = Random.Range(rangeMin, rangeMax);
+
+            // Instantiate the Obstacle.
+            GameObject selectedObstacle = obstaclesList[randomIndex];
+            if (obstaclesList[randomIndex] != null)
             {
-                GameObject obstacleInstance = Instantiate(selectedObstacle, targetPosition, Quaternion.identity);
+                GameObject obstacleInstance = Instantiate(selectedObstacle, targetPosition + obstaclesPosition[randomPosition], Quaternion.identity);
             }
+
+            // Change the Random.Range for the next obstacle position
             rangeMin += 3;
             rangeMax += 3;
         }
